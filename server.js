0215fa692daf303d4ceb6d6eeb8f9ebbcb73a17c
Jeana Rose <jr.mathis608@gmail.com>
2021-01-22 12:56:30 -0500

    Added middleware and connection

--- server.js
+++ server.js
@@ -9,3 +9,27 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on htt;://localhost:${PORT}`);
 });
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "moose",
+  database: "watchlist_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
