87579ea70e9265e786961cf80243c9f26d3cab61
Jeana Rose <jr.mathis608@gmail.com>
2021-01-22 12:55:43 -0500

    Built server

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mysql = require("mysql");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on htt;://localhost:${PORT}`);
+});
