c4e06a146016eec5a220e3e6075e6621c4323ed5
Jeana Rose <jr.mathis608@gmail.com>
2021-01-22 13:18:00 -0500

    Added a seeds file

---
+++ seeds.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS watchl_ist_db;
+
+CREATE DATABASE watch_list_db;
+
+USE watch_list_db;
+
+CREATE TABLE movies (
+  id int NOT NULL AUTO_INCREMENT,
+  movie varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO movies (movie)
+VALUES ("Frozen 2");
