51fe2babf6af64372e33fbd1f264992a42ea04b7
Jeana Rose <jr.mathis608@gmail.com>
2021-01-22 13:15:58 -0500

    Added a connection query inside of the root route. Added content to index.handlebars

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mysql = require("mysql");
 
 const app = express();
 
+// PORT to listen on 
 const PORT = process.env.PORT || 8080;
 
 // Middleware
@@ -30,9 +32,19 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// VIEWS ROUTES
+
 app.get("/", (req, res) => {
-  res.send("All my movies will go here.");
+//   res.send("All my movies will go here.");
+    connection.query(`SELECT * FROM movies`,(err, data)=>{
+        if(err) throw err;
+        res.render("index", { movies: data });
+    })
 });
+
+app.get("/movies/new", (req, res) => {
+    res.send("A form to create a new movie will go here.");
+  });
 
 app.get("/movies/:id", (req, res) => {
   res.send("A single movie will go here.");
@@ -42,9 +54,7 @@ app.get("/movies/:id/edit", (req, res) => {
   res.send("A form to update the movie will go here.");
 });
 
-app.get("/movies/new", (req, res) => {
-  res.send("A form to create a new movie will go here.");
-});
+// API ROUTES
 
 app.post("/api/movies", (req, res) => {
   res.send(
@@ -62,6 +72,7 @@ app.delete("/api/movies/:id", (req, res) => {
   );
 });
 
+// Listen on the PORT
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
